59c2300c8810e09127f424c720a20f98330b8df3
don-E Merson <dmerson@email.arizona.edu>
2018-05-18 07:38:12 -0700

    Got the analysis part for r1 and r2 working and deployed to each worksheet.

--- Code/SQL/Worksheets/Algo 4 Worksheet.sql
+++ Code/SQL/Worksheets/Algo 4 Worksheet.sql
@@ -101,7 +101,6 @@ WHERE AlgorithmId = @algorithmid
       AND MaxApplicants = @MaxApplicants
       AND MinimumAward = @MinimumAward
       AND MaximumAward = @MaximumAward AND AwardingGroupId=@awardgroup;
-
 SELECT ApplicantId,
        SUM(Award) Total
 FROM dbo.ScholarshipAwards
@@ -110,4 +109,109 @@ WHERE AlgorithmId = @algorithmid
       AND MinimumAward = @MinimumAward
       AND MaximumAward = @MaximumAward AND AwardingGroupId=@awardgroup
 GROUP BY ApplicantId
-ORDER BY Total DESC;
+ORDER BY Total desc;
+
+DELETE FROM dbo.ScholarshipAwardAnalysises
+WHERE AlgorithmId = @algorithmid
+      AND MaxApplicants = @MaxApplicants
+      AND MinimumAward = @MinimumAward
+      AND MaximumAward = @MaximumAward
+      AND AwardingGroupId = @awardgroup;
+
+;WITH calculations
+AS (SELECT ApplicantRankings.AwardingGroupId,
+           Ranking,
+           SUM(Award) Total,
+           ISNULL(LEAD(Ranking) OVER (ORDER BY Ranking), Ranking + 1) NextRanking,
+           ISNULL(LEAD(SUM(Award)) OVER (ORDER BY Ranking), 0) NextAmount
+    FROM dbo.ScholarshipAwards
+        INNER JOIN dbo.ApplicantRankings
+            ON ApplicantRankings.ApplicantId = ScholarshipAwards.ApplicantId
+               AND ApplicantRankings.AwardingGroupId = ScholarshipAwards.AwardingGroupId
+    WHERE AlgorithmId = @algorithmid
+          AND MaxApplicants = @MaxApplicants
+          AND MinimumAward = @MinimumAward
+          AND MaximumAward = @MaximumAward
+          AND ApplicantRankings.AwardingGroupId = @awardgroup
+    GROUP BY ApplicantRankings.AwardingGroupId,
+             Ranking),
+      ra1checkraw
+AS (SELECT calculations.AwardingGroupId,
+           calculations.Ranking,
+           calculations.Total,
+           calculations.NextRanking,
+           calculations.NextAmount,
+           CASE
+               WHEN calculations.Ranking + 1 = calculations.NextRanking THEN
+                   1
+               ELSE
+                   0
+           END OrderPreserved,
+           CASE
+               WHEN Total > calculations.NextAmount THEN
+                   1
+               ELSE
+                   0
+           END AmountPreserved
+    FROM calculations),
+      ra1
+AS (SELECT ra1checkraw.AwardingGroupId,
+           MIN(ra1checkraw.OrderPreserved) + MIN(ra1checkraw.AmountPreserved) CHECKer,
+           CASE
+               WHEN MIN(ra1checkraw.OrderPreserved) + MIN(ra1checkraw.AmountPreserved) = 2 THEN
+                   1
+               ELSE
+                   0
+           END r1Check
+    FROM ra1checkraw
+    GROUP BY ra1checkraw.AwardingGroupId),
+      countranks
+AS (SELECT calculations.AwardingGroupId,
+           COUNT(Ranking) cnt,
+           MAX(Ranking) maxrank
+    FROM calculations
+    GROUP BY calculations.AwardingGroupId),
+      ra2table
+AS (SELECT countranks.AwardingGroupId,
+           CASE
+               WHEN countranks.cnt = maxrank THEN
+                   1
+               ELSE
+                   0
+           END ra2check
+    FROM countranks)
+INSERT INTO dbo.ScholarshipAwardAnalysises
+(
+    AwardingGroupId,
+    AlgorithmId,
+    MaximumAward,
+    MinimumAward,
+    MaxApplicants,
+    RA1,
+    RA2
+)
+SELECT TOP 1
+       countranks.AwardingGroupId,
+       @algorithmid AlgorithmId,
+       @MaximumAward MaximumAward,
+       @MinimumAward MinimumAward,
+       @MaxApplicants MaxApplicants,
+       ra1.r1Check,
+       ra2table.ra2check
+FROM ra1checkraw
+    INNER JOIN ra1
+        ON ra1.AwardingGroupId = ra1checkraw.AwardingGroupId
+    INNER JOIN countranks
+        ON countranks.AwardingGroupId = ra1.AwardingGroupId
+    INNER JOIN ra2table
+        ON ra2table.AwardingGroupId = ra1checkraw.AwardingGroupId;
+
+
+
+SELECT *
+FROM dbo.ScholarshipAwardAnalysises
+WHERE AlgorithmId = @algorithmid
+      AND MaxApplicants = @MaxApplicants
+      AND MinimumAward = @MinimumAward
+      AND MaximumAward = @MaximumAward
+      AND AwardingGroupId = @awardgroup;
