3548f55567079da4994bc65e433ff354f237ec8c
don-E Merson <dmerson@email.arizona.edu>
2018-07-11 19:44:38 -0700

    Added multi use intelligent get analysis

---
+++ Code/SQL/RedGate/Stored Procedures/dbo.RunAnalysis.sql
@@ -0,0 +1,59 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROC [dbo].[RunAnalysis] 
+	@AwardingGroupId INT,
+    @MaximumAward DECIMAL(9, 2),
+    @MinimumAward DECIMAL(9, 2),
+    @MaxApplicants INT,
+	@RunAnalysisFirst BIT=0
+AS
+DECLARE @whichTable CHAR(1);
+SET @whichTable =
+(
+    SELECT TOP 1
+           *
+    FROM
+    (
+        SELECT TOP 1
+               '1' IsNormalized
+        FROM dbo.NormalizedView
+        WHERE AwardingGroupId = @AwardingGroupId
+        UNION
+        SELECT TOP 1
+               '0' IsNormalized
+        FROM dbo.DenormalizedEntries
+        WHERE AwardingGroupId = @AwardingGroupId
+        UNION
+        SELECT -1 IsNormalized
+    ) whichTable
+    ORDER BY 1 DESC
+);
+IF @whichTable =1
+BEGIN
+	IF @RunAnalysisFirst =1
+	BEGIN
+		EXEC dbo.RunAllAlgorithms @AwardingGroupId ,      -- int
+		                          @MaximumAward , -- decimal(9, 2)
+		                          @MinimumAward , -- decimal(9, 2)
+		                          @MaxApplicants     -- int
+		
+	end
+	SELECT * FROM dbo.ScholarshipAwardAnalysises
+	WHERE AwardingGroupId =@AwardingGroupId
+END
+IF @whichTable =0
+BEGIN
+	IF @RunAnalysisFirst =1
+	BEGIN
+		EXEC dbo.RunAllDenormalizedAlgorithms @AwardingGroupId,      -- int
+		                                      @MaximumAward , -- decimal(9, 2)
+		                                      @MinimumAward , -- decimal(9, 2)
+		                                      @MaxApplicants     -- int
+		
+	end
+	SELECT * FROM dbo.DenormalizedEntryAnalysises
+	WHERE AwardingGroupId =@AwardingGroupId
+end
+GO
