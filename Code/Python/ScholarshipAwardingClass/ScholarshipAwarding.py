2d9fd4a9961209b583405d8957dd5af64b046de3
dmerson <dmerson@email.arizona.edu>
2018-07-16 14:43:15 -0700

    Added the rest of the sps from the worksheet after verified the format was working (Closing the sql connection)

--- Code/Python/ScholarshipAwardingClass/ScholarshipAwarding.py
+++ Code/Python/ScholarshipAwardingClass/ScholarshipAwarding.py
@@ -38,3 +38,53 @@ class ScholarshipAwarding(object):
         analysis= pandas.io.sql.read_sql(sql, self.GetConnection())
         self.GetConnection().close()
         return (analysis)
+
+    def InsertDenormalizedData(self,awarding_group_id, scholarshipName, scholarshipAmount, applicantName, applicantRank):
+        sql = "InsertIntoDenormalizedEntry " + str(awarding_group_id) + ","
+        sql = sql + "'" + str(scholarshipName) + "',"
+        sql = sql + str(scholarshipAmount) + ","
+        sql = sql + "'" + str(applicantName) + "',"
+        sql = sql + str(applicantRank)
+        # return sql
+        results= pandas.io.sql.read_sql(sql, self.GetConnection())
+        self.GetConnection().close()
+        return (results)
+
+    def CreateAnalysisFromSpreadsheet(self,awarding_group_name, filePath, maximum_award, minimum_award, max_applicants,
+                                         run_analysis_first):
+        new_awarding_group = pandas.io.sql.read_sql("CreateAwardingGroup 'FromPython'", self.GetConnection())
+        new_awarding_group_id = new_awarding_group["AwardingGroupId"][0]
+        from pandas import ExcelWriter
+        from pandas import ExcelFile
+        df = pd.read_excel(filePath, sheetname='Sheet1')
+        for i in df.index:
+            scholarship = df["Scholarship"][i]
+            scholarship_award = df["ScholarshipAward"][i]
+            applicant = df["Applicant"][i]
+            applicant_ranking = df["ApplicantRanking"][i]
+            self.InsertDenormalizedData(new_awarding_group_id, scholarship, scholarship_award, applicant, applicant_ranking)
+        analysis =self.GetAnalysis(new_awarding_group_id, maximum_award, minimum_award, max_applicants, run_analysis_first)
+        self.GetConnection().close()
+        return (analysis)
+
+    def GetScholarshipAwards(self,algorithm_id, awading_group_id, maximum_award, minimum_award, max_applicants):
+        sql = "GetScholarshipAwards " + str(algorithm_id) + ","
+        sql = sql + str(awading_group_id) + ","
+        sql = sql + str(maximum_award) + ","
+        sql = sql + str(minimum_award) + ","
+        sql = sql + str(max_applicants)
+        # return sql
+        results= pandas.io.sql.read_sql(sql, self.GetConnection())
+        self.GetConnection().close()
+        return (results)
+
+    def GetDeonormalizedScholarshipAwards(self,algorithm_id, awading_group_id, maximum_award, minimum_award, max_applicants):
+        sql = "GetDeonormalizedScholarshipAwards " + str(algorithm_id) + ","
+        sql = sql + str(awading_group_id) + ","
+        sql = sql + str(maximum_award) + ","
+        sql = sql + str(minimum_award) + ","
+        sql = sql + str(max_applicants)
+        # return sql
+        results = pandas.io.sql.read_sql(sql, self.GetConnection())
+        self.GetConnection().close()
+        return (results)
