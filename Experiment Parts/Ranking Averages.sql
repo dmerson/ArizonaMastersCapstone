1cad054052b6eaa5043e02263d8840d0a151ae05
don-E Merson <dmerson@email.arizona.edu>
2018-07-22 13:35:47 -0700

    Set back sp to most important purpose.

--- Experiment Parts/Ranking Averages.sql
+++ Experiment Parts/Ranking Averages.sql
@@ -14,26 +14,26 @@ AS (SELECT AwardingGroups.AwardingGroupName,
             ON AwardingGroups.AwardingGroupId = DenormalizedEntryAnalysises.AwardingGroupId
     WHERE MaximumAward = 12400
           AND MaxApplicants = 2
-          AND MinimumAward = 250),
+          AND MinimumAward = 250
+		--  AND MiniumumAwarded >=200
+		  ),
      cntofffirstranked
 AS (SELECT *
     FROM rankings
-    WHERE rankings.Ranking = 1),
+    WHERE rankings.Ranking = 1
+	),
      totaloftopranked
 AS (SELECT cntofffirstranked.AlgorithmId,
            COUNT(cntofffirstranked.Ranking) cntOf1stRanked
     FROM cntofffirstranked
-    GROUP BY cntofffirstranked.AlgorithmId)
+    GROUP BY cntofffirstranked.AlgorithmId),
+	avgranking AS (
 SELECT rankings.AlgorithmId,
        AVG(CONVERT(DECIMAL, rankings.Ranking)) avgRank
 FROM rankings
-GROUP BY rankings.AlgorithmId
-ORDER BY avgRank;
+GROUP BY rankings.AlgorithmId)
 
-
---SELECT * FROM dbo.DenormalizedEntryAnalysises
--- INNER JOIN dbo.AwardingGroups ON AwardingGroups.AwardingGroupId = DenormalizedEntryAnalysises.AwardingGroupId
---WHERE MaximumAward=12400 AND MaxApplicants=2 AND MinimumAward =250 
---AND AwardingGroupName ='aw1'
---  ORDER BY RA1 DESC, RA2 DESC, RA3 DESC, UniqueAwardees DESC, MaximumAward DESC, NumberOfAwarded desc
+SELECT * FROM totaloftopranked
+ORDER BY 2 desc
+--ORDER BY avgRank;
 
